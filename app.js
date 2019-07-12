12683f3e9d0995b36314967f5c91a28a703f4ecc
Shahed Mehbub <shahed739@gmail.com>
2019-07-13 03:58:50 +0600

    Added darksky and skycons api

--- app.js
+++ app.js
@@ -1,15 +1,59 @@
-window.addEventListener('load', () => {
-    let lat;
-    let long;
+window.addEventListener("load", () => {
+  let lat;
+  let long;
+  let temperatureDescription = document.querySelector(
+    ".temperature-description"
+  );
+  let temperatureDegree = document.querySelector(".temperature-degree");
+  let locationTimezone = document.querySelector(".location-timezone");
+  let temperatureSection = document.querySelector(".temperature-section");
+  const temperatureSpan = document.querySelector(".temperature-section span");
 
-    if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(position => {
-            // console.log(position);
-            lat = position.coords.latitude;
-            long = position.coords.longitude;
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(position => {
+      // console.log(position);
+      lat = position.coords.latitude;
+      long = position.coords.longitude;
+
+      const proxy = "https://cors-anywhere.herokuapp.com/";
+      const api = `${proxy}https://api.darksky.net/forecast/b4536f775f1b6d338ba0e1f84be6c414/${lat},${long}`;
+
+      fetch(api)
+        .then(response => {
+          return response.json();
+        })
+        .then(data => {
+          //console.log(data.currently);
+          const { temperature, summary, icon } = data.currently;
+          // Set DOM Elements from the API
+          temperatureDegree.textContent = temperature;
+          temperatureDescription.textContent = summary;
+          locationTimezone.textContent = data.timezone;
+          // Formula for Celsius
+          let celsius = (temperature - 32) * (5 / 9);
+          // Set Icon
+          setIcons(icon, document.querySelector(".icon"));
+
+          // Change temperature to Celsius-Farenheit
+          temperatureSection.addEventListener("click", () => {
+            if (temperatureSpan.textContent === "F") {
+              temperatureSpan.textContent = "C";
+              temperatureDegree.textContent = Math.floor(celsius);
+            } else {
+              temperatureSpan.textContent = "F";
+              temperatureDegree.textContent = temperature;
+            }
+          });
         });
-    }
-    else{
-        h1.textContent = "Location not found";
-    }
+    });
+  } else {
+    h1.textContent = "Location not found";
+  }
+
+  function setIcons(icon, iconID) {
+    const skycons = new Skycons({ color: "white" });
+    const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+    skycons.play();
+    return skycons.set(iconID, Skycons[currentIcon]);
+  }
 });
